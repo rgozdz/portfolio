d00be18f15e5ebcf04f8df4c6b691f195a76f741
Rafal Gozdz <rag@scalepoint.com>
2020-11-09 15:11:12 +0100

    portfolio update

--- script.js
+++ script.js
@@ -13,7 +13,18 @@ const aboutBtn = document.querySelector("a[href='#about']");
 aboutBtn.addEventListener('click' ,(e)=>{
     e.preventDefault();
     var aboutSectionHeight = document.querySelector(".about");
-    aboutSectionHeight.scrollIntoView();
+    aboutSectionHeight.scrollIntoView({
+        behavior: 'smooth',
+    });
+})
+
+const projectsBtn = document.querySelector("a[href='#projects']");
+projectsBtn.addEventListener('click' ,(e)=>{
+    e.preventDefault();
+    var aboutSectionHeight = document.querySelector(".projects");
+    aboutSectionHeight.scrollIntoView({
+        behavior: 'smooth'
+    });
 })
 
 const homeBtn = document.querySelector("a[href='#introduction']");
@@ -27,7 +38,9 @@ const arrow = document.querySelector(".arrow");
 arrow.addEventListener('click' ,(e)=>{
     e.preventDefault();  
     var aboutSectionHeight = document.querySelector(".about");
-    aboutSectionHeight.scrollIntoView(); 
+    aboutSectionHeight.scrollIntoView({
+        behavior: 'smooth'
+    }); 
 });
 
 function scrollInto(scrollHeight) {
@@ -48,6 +61,31 @@ function changeTitle() {
     
     activeElement ++;
 }
+
+var shortage = document.querySelector('.shortage'),
+skils = document.querySelector('.skils'),
+about = document.querySelector('.about');
+
+window.addEventListener("scroll", function(){
+  
+    var aboutOffset =about.offsetHeight;
+    var windowsOffset = window.pageYOffset;
+    var offsetDiff = aboutOffset -windowsOffset;
+
+    if(offsetDiff< aboutOffset/2){
+        shortage.style.opacity =1;
+        skils.style.opacity =1;
+        shortage.style.transform = "translateX(-100%)";
+        skils.style.transform = "translateX(50%)";
+    }
+    else {
+        shortage.style.opacity =0;
+        skils.style.opacity =0;
+        shortage.style.transform = "translateX(100%)";
+        skils.style.transform = "translateX(-100%)";
+    }
+
+  })
 
 setTimeout(changeTitle, 1500);
 setTimeout(() => {setInterval(changeTitle, 3000)}, 1500);
